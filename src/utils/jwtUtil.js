9d5e1e7232b19933320ebabc3b2ba5240d37fa25
unknown <muofunanyajennifer1@gmail.com>
2023-08-09 23:05:30 +0100

    worked on token verification

---
+++ src/utils/jwtUtil.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken');
+const jwt_decode = require('jtw-decode')
+require('dotenv').config()
+
+// encode the jwt
+const encode_jwt = function (obj) {
+    const jwtToken = jwt.sign(obj,
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' }
+    );
+    return jwtToken;
+}
+
+// verify token
+const verifyToken = (token) => {
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        return {
+            expired: false,
+            decoded,
+        }
+    } catch(err){
+        return{
+            expired: err.message === 'JWT expired',
+            decoded: null
+        };
+    }
+}
+// decde the token
+const decodeToken = (token) => {
+    const id = jwt_decode(token)._id
+    return id
+}
+
+module.eports = { encode_jwt, verifyToken, decodeToken}
